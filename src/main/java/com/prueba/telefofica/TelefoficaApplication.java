4d48aa430a1fdbb6f37c03c5090de2da873528f9
Jose David Castillo <jose1xdd@gmail.com>
2024-10-19 11:44:54 -0500

    creacion del proyecto

---
+++ src/main/java/com/prueba/telefofica/TelefoficaApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.telefofica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelefoficaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelefoficaApplication.class, args);
+	}
+
+}
